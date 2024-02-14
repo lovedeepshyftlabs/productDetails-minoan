30466e7717879dd3ded311f0a9ad2893df23f871
lovedeepshyftlabs <lovedeep@shyftlabs.io>
2024-02-14 21:06:13 +0530

    modal-creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
-import ProductDetails from './components/productDetails';
+import "./App.css";
+import { useState } from "react";
+import ProductDetails from "./components/productDetails";
+import { Button, Drawer } from "antd";
+
 function App() {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
   return (
-    <ProductDetails product_id="8268418646255" store_sync_id="356555257157305024" />
+    <>
+      <Button type="primary" onClick={showDrawer}>
+          Open
+      </Button>
+      <Drawer
+        title={<h1>Product details</h1>}
+        placement={"right"}
+        width={"100%"}
+        onClose={onClose}
+        open={open}
+      >
+        <ProductDetails product_id="8268418646255" store_sync_id="356555257157305024" setOpen={setOpen}/>
+      </Drawer>
+    </>
   );
 }
 
